b22faf658c92f0269b055b18ad4eeb208dc6001f
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-05-22 18:21:29 +0530

    Ex1: Added cost compute function

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -20,3 +20,30 @@ plt.xlabel('Population of City in 10,000s')
 plt.ylabel('Profit in $10,000s')
 plt.show()
 
+
+# ## 2. Gradient Descent
+
+# In[3]:
+
+m = y.shape
+# obtain number of training samples
+m = m[0]
+x = np.array([x])
+# use hstack to concatenate to column vector-like arrays, obtain a 2-column matrix
+X = np.hstack((np.ones((m, 1)), x.T))
+
+# Cost computing function
+def compute_cost(X, Y, theta):
+    J = 0
+    # H = theta(0) + theta(1)*x(1) + theta(2)*x(2)...
+    H = np.dot(X, theta)
+
+    # Handy way to obtain the squares
+    J = np.dot((H - Y).T, (H - Y))
+    J = J / (2 * m)
+    return float(J)
+
+Y = np.array([y]).T
+theta = np.zeros((2, 1))
+print "Cost with theta = [0, 0]:", compute_cost(X, Y, theta)
+
