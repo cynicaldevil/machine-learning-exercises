909cd47d9011d342af898c80a03991ddd07aa7dd
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-06-16 00:22:06 +0530

    Added feed forward propogation function to predict output from given weights

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -95,7 +95,7 @@ def regularized_gradient(theta, X, Y):
 
 # ## 6.1 Obtain  Θ (big theta)
 
-# In[21]:
+# In[7]:
 
 import scipy.optimize as opt
 
@@ -125,7 +125,7 @@ theta = get_theta()
 
 # ## 6.2 Prediction
 
-# In[20]:
+# In[8]:
 
 def predict_one_vs_all(theta, X):
     correct_hypotheses = 0
@@ -143,3 +143,52 @@ def predict_one_vs_all(theta, X):
     print 'Accuracy of One vs. All classifier trained via logisitic regression:     %0.2f%% \n(Actual value depends on iterations on optimization function) \n    ' % (float(correct_hypotheses) * 100.0 / 5000.0)
 
 predict_one_vs_all(theta, X)
+
+
+# # 7. Neural Networks
+
+# In[42]:
+
+data_nn = scipy.io.loadmat('data/ex3weights.mat')
+theta1 = data_nn['Theta1']
+theta2 = data_nn['Theta2']
+
+
+# ## 7.1 Prediction function
+
+# In[41]:
+
+def prediction(hypothesis, i, Y):
+    prediction = np.argmax(hypothesis) + 1
+    if (prediction == Y[i]):
+        return True
+    else:
+        return False
+
+
+# ## 7.2 Feed Forward Propogation
+
+# In[40]:
+
+def feed_forward_propogation(X, Y):
+    correct_hypotheses = 0
+    for i in range(0, X.shape[0]):
+        # First layer
+        z2 = np.dot(theta1, X[i].T)
+        a2 = expit(z2)
+        # Add bias nodes
+        a2 = np.concatenate((np.ones(1), a2), axis=0)
+
+        # Second layer
+        z3 = np.dot(theta2, a2.T)
+        a3 = expit(z3)
+
+        # Final layer (hypothesis)
+        hypothesis = a3
+        result = prediction(hypothesis, i, Y)
+        if (result):
+            correct_hypotheses = correct_hypotheses + 1
+
+    print 'Accuracy of Neural Network: %0.2f%%' % (float(correct_hypotheses) * 100.0 / 5000.0)
+
+feed_forward_propogation(X, Y)
