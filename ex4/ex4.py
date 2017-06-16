8389ccae25b291ea094b3bea90d7a86e8fe496f5
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2017-06-16 16:29:44 +0530

    Display selection of images

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -3,7 +3,7 @@
 
 # # 1. Import images
 
-# In[5]:
+# In[8]:
 
 import scipy.io
 data = scipy.io.loadmat('data/ex3data1.mat')
@@ -14,5 +14,26 @@ X = data['X']
 # A 5000-dimensional matrix, each element representing the number shown in the corresponding image
 Y = data['y'].reshape(5000)
 
-print Y.shape
+
+# # 2. Display selection of images
+
+# In[7]:
+
+import numpy as np
+import random
+from matplotlib import pyplot as plt
+
+img_arr = np.empty([20, 220])
+for j in range(0, 10):
+    img_row = np.empty([20, 20])
+    for i in range(0, 10):
+        img = X[random.randint(1, 5000)].reshape(20, 20)
+        img_row = np.concatenate((img_row, img), axis=1)
+    img_arr = np.concatenate((img_arr, img_row), axis=0)
+
+plt.figure(figsize=(12,8))
+plt.imshow(img_arr[20:, 20: ].T, interpolation="nearest", cmap='gray')
+
+# Show random selection of 100 images using imshow
+plt.show()
 
